fe414813b35a3f70b099bd865e69bcf053bdb558
Erik Desjardins <erikdesjardins@users.noreply.github.com>
2018-03-30 23:43:33 -0400

    Create webpack.config.js

---
+++ webpack.config.js
@@ -0,0 +1,42 @@
+const path = require('path');
+
+const InertEntryPlugin = require('inert-entry-webpack-plugin');
+const ZipPlugin = require('zip-webpack-plugin');
+const rollupCommonjsPlugin = require('rollup-plugin-commonjs');
+const rollupReplacePlugin = require('rollup-plugin-re');
+
+module.exports = {
+	entry: 'extricate-loader!interpolate-loader!./src/manifest.json',
+	output: {
+		path: path.join(__dirname, 'dist'),
+		filename: 'manifest.json',
+	},
+	module: {
+		rules: [{
+			test: /\.entry\.js$/,
+			use: [
+				{ loader: 'file-loader', options: { name: '[name].js' } },
+				{
+					loader: 'webpack-rollup-loader',
+					options: {
+						plugins: [
+							rollupCommonjsPlugin({ extensions: ['.png'] }),
+						],
+					},
+				},
+			],
+		}, {
+			test: /\.(png)$/,
+			use: [
+				{ loader: 'file-loader', options: { name: '[name].[ext]' } },
+			],
+		}],
+	},
+	optimization: {
+		minimize: false,
+	},
+	plugins: [
+		new InertEntryPlugin(),
+		new ZipPlugin({ filename: 'TLD.zip' }),
+	],
+};
